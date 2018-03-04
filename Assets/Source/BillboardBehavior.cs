aa3553b472783c3f3856711e1d2df8965c69db5e
Josh Schonstal <josh@schonstal.org>
2018-03-04 10:44:21 -0800

    🖼 Add BillboardBehavior

---
+++ Assets/Source/BillboardBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardBehavior : MonoBehaviour {
+  public Transform cameraTransform;
+
+  void Start() {
+    if (cameraTransform == null) {
+      cameraTransform = Camera.main.transform;
+    }
+  }
+
+  void LateUpdate() {
+    this.transform.forward = cameraTransform.forward;
+  }
+}
